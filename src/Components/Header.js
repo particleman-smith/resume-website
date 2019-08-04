e15a4f81be17479c0421672746d654315bfda2f2
Unknown <particleman.smith@gmail.com>
2019-08-03 22:18:47 -0600

    Additional theme/font changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -34,8 +34,8 @@ class Header extends Component {
 
       <div className="row banner">
          <div className="banner-text">
-            <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
+            <h2 className="banner-text">Hello, I'm</h2>
+            <h1 className="responsive-headline">{name}.</h1>
             <hr />
             <ul className="social">
                {networks}
